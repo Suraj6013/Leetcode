14a0177907cc9bfecca3a76cb63c6189f32cb2d6
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-05-13 23:58:42 +0530

    used stack

--- LC1249.py
+++ LC1249.py
@@ -1,27 +1,27 @@
 def parantheses(string):
     stack = []
     result = ''
-    
-    for char in string:
+    open_indices = []
+
+    for i, char in enumerate(string):
         if char == '(':
-            stack.append(char)
+            stack.append(i)
             result += char
+            open_indices.append(len(result) - 1)
         elif char == ')':
             if stack:
                 stack.pop()
                 result += char
+                open_indices.pop() if open_indices else None
             else:
-                return "Unbalanced parentheses"
-        elif char == '[' or char == ']':
-
-            continue
+                continue
         else:
             result += char
-    
-    if stack:
-        return "Unbalanced parentheses"
-    else:
-        return result
+
+    for index in reversed(open_indices):
+        result = result[:index] + result[index+1:]
+
+    return result
 
 input_string = "((a+b)-[c*d]+e"
 output_string = parantheses(input_string)
