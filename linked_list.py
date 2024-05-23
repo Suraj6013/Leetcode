309a320b2123fb99b146432fc7d188dea68aa190
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-05-23 21:49:24 +0530

    added menu

--- linked_list.py
+++ linked_list.py
@@ -23,9 +23,23 @@ class LinkedList:
             current = current.next
         print()
 
-# Usage
-ll = LinkedList()
-ll.insert(1)
-ll.insert(2)
-ll.insert(3)
-ll.print_list()  # Output: 1 2 3
+# Menu
+def menu():
+    ll = LinkedList()
+    while True:
+        print("\n1. Insert")
+        print("2. Print")
+        print("3. Exit")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            data = int(input("Enter the data to insert: "))
+            ll.insert(data)
+        elif choice == 2:
+            ll.print_list()
+        elif choice == 3:
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+menu()
