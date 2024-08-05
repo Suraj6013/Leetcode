f4a49a35c7f0801e0cad2dfdec4adc638e3ddbbe
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-08-05 17:47:55 +0530

    init

--- hackerrank/airoplane_algorithm.py
+++ hackerrank/airoplane_algorithm.py
@@ -4,10 +4,23 @@ distance = list(map(int, user_input.split()))
 F = int(input())
 user_input = input()
 fuels = list(map(int, user_input.split()))
+
+
 AtoB = int(input())
 E = int(input())
 new_distance = [distance[0]]
 for i in range(len(distance)-1):
     x = distance[i+1]-distance[i]
     new_distance.append(x)
-print(new_distance)
+
+
+for i in range(len(fuels)):
+    E = E - new_distance[i]    
+    E = E + fuels[i]     
+    if E >= AtoB:
+        print(i)
+        break
+    else:
+        print("-1")
+        break
+
