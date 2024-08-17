64b617a35f842974043ebd25faa71c9a85540a30
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-08-17 22:50:15 +0530

    Implement binary search algorithm in rotated sorted array

---
+++ 33. Search in Rotated Sorted Array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            
+            # Check if the left half is sorted
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # Otherwise, the right half must be sorted
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
+    
+solution = Solution()
+print(solution.search([4,5,6,7,0,1,2], 0)) # 4
