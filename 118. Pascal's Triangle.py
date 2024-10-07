c9d82e21fc0fd3556c62e0bf797ee78e30bc6808
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-10-07 23:36:20 +0530

    solution to generate pascal's triangle

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -7,8 +7,7 @@ class Solution:
         pascal = [[1]] 
         for i in range(1, numRows):
             row = [1]  
-            for j in range(1, i):
-                
+            for j in range(1, i):                
                 row.append(pascal[i-1][j-1] + pascal[i-1][j])
             row.append(1)  
             pascal.append(row)
